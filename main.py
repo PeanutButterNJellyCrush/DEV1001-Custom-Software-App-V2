ab35eb511dbdb601e7925d0c52dc407e68b8839f
Bonnie <bonniechiu527@gmail.com>
2024-12-19 22:40:35 +1000

    commit

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import typer
 
 
 def main():
-    person_name = typer.prompt("What's your name?")
-    print(f"Hello {person_name}")
+    user_name_input = typer.prompt("What's your name?")
+    print(f"Hello {user_name_input}")
+    
 
 
 if __name__ == "__main__":
