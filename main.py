ed3ce38a526502bc8ae0debca0f6e23cc1ddeeec
Bonnie <bonniechiu527@gmail.com>
2024-12-19 22:45:48 +1000

    commit

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import typer
 
 def main():
     user_name_input = typer.prompt("What's your name?")
-    print(f"Hello {user_name_input}")
-    
+    print(f'Hello {user_name_input}')
+    user_pin_input = typer.prompt('Enter PIN')
+    typer.echo(user_pin_input)
+
 
 
 if __name__ == "__main__":
