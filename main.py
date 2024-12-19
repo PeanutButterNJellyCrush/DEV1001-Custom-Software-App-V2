19fda50000fe07627c18e6fede5d08b7f394f938
Bonnie <bonniechiu527@gmail.com>
2024-12-19 23:47:14 +1000

    commit

--- main.py
+++ main.py
@@ -8,18 +8,29 @@ def main():
     user_pin_input = typer.prompt('Enter PIN')
     typer.echo(user_pin_input)
     #user_pin_input=hashlib.sha256(user_pin.encode()).hexdigest()           hashedpin
+    
+    # user_credentials = {
+    #     'Name:':user_pin_input,
+    #     'user_pin_input':user_pin_input
+    # }
     user_credentials = {
-        'Name:':user_pin_input,
-        'user_pin_input':user_pin_input
+        'name':'bonnie',
+        'pin': 'd'
     }
-    try:
-        with open('user_name_pin_data','r', encoding='utf-8') as file:
-            user_credentials = json.load(file)
-    except FileNotFoundError:
-        user_name_pin_data.append(user_credentials)
+        #open file to check key-value pair from json data
+    # with open('user_name_pin_data','r', encoding='utf-8') as file:
+    #         user_credentials = json.load(file)
+            
+    for name in user_credentials:
+        if name['name']=='bonnie' and name['pin']=='d':
+            print('success')
+            
+   
+    # except FileNotFoundError:
+    #     user_name_pin_data.append(user_credentials)
         
-    with open('user_name_pin_data','w', encoding='utf-8') as file:
-        json.dump(user_credentials, file, indent=4)
+    # with open('user_name_pin_data','w', encoding='utf-8') as file:
+    #     json.dump(user_credentials, file, indent=4)
         
         
         
