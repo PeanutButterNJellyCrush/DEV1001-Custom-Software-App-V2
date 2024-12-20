bc0e510fde6167971dbd4801b8133e4f7b9dd6c4
Bonnie <bonniechiu527@gmail.com>
2024-12-20 15:41:44 +1000

    exit typer library

--- main.py
+++ main.py
@@ -1,41 +1,50 @@
-import typer
 import json
 from datetime import date
 
 
 def main():
-    '''Prompt for name and PIN
-    '''
-    user_name_input = typer.prompt('Enter your name')
-    user_pin_input = typer.prompt('Enter PIN')
-    typer.echo(f'{user_name_input}:{user_pin_input}')
+    
+    user_name_input = str(input('Enter your name'))
+    user_pin_input = int(input('Enter PIN'))
     
     #Dictionary for user_name & user_pin
-    user_credentials = {
+    user_credentials_input = {
         'Name:':user_name_input,
-        'user_pin_input':user_pin_input
+        'PIN':user_pin_input
     }
-    #check if user file exists, if not,m create it with empty state
-    with open('user_credentials','w', encoding='utf-8') as file:
-        json.dump(user_credentials, file, indent=4)
-    
-    # load user file with state
-    with open('user_credentials','r', encoding='utf-8') as file:
-        user_credentials = json.load(file)
+    print(user_credentials_input)
+
+    user_credentials = {
+    }
+        
+    try:     
+    #check if user file exists      
+        with open('user_credentials.json','r', encoding='utf-8') as file:
+            user_credentials = json.load(file)
             
+        if user_credentials == user_credentials_input:
+            print('login details present')
+            with open('user_credentials.json','w', encoding='utf-8') as file:
+                    json.dump(user_credentials, file, indent=4)    
+        else:
+            print('no valid login details.')
+    #if not, create one
+    except FileNotFoundError:
+        print('error')
+
 
     #main menu options
-    typer.echo('You have [calories]/{calories_daily_target} today.')
-    typer.echo('You have [calories]/[target] this week')
-    typer.echo('Options:\n 1.Set calories target \n 2.Add calorie entry \n 3.Help \n 4.Quit')
+    print('You have [calories]/ today.')
+    print('You have [calories]/[target] this week')
+    print('Options:\n 1.Set calories target \n 2.Add calorie entry \n 3.Help \n 4.Quit')
             
-    choice = typer.prompt('Enter option', type=int)
+    choice = int(input('Enter option'))
     
     match choice:
         case 1:
-            typer.echo('Set calorie target')
+            print('Set calorie target')
             #set calories target & save to json file
-            calorie_daily_target = typer.prompt('Set your daily target calorie', type=int)
+            calorie_daily_target = int(input('Set your daily target calorie'))
             today = date.today().isoformat()
             
             calorie_daily_target_data = {
@@ -47,22 +56,29 @@ def main():
             with open('calories_daily_target','r', encoding='utf-8') as file:
                 calorie_daily_target_data = json.load(file)
                 print(calorie_daily_target_data)
+                
             #iterate though data to check
-                for entry in calorie_daily_target_data:
-                    if entry.get('Name') == user_name_input:
-                        if 'Calories daily' in entry:
-                            return True, entry['Calories daily']
-                        else: #if not found, write into file
-                            with open('calories_daily_target','w', encoding='utf-8') as file:
-                                json.dump(calorie_daily_target_data, file, indent=4)
+            for entry in calorie_daily_target:
+                print(entry)
+            for entry in calorie_daily_target.entry():
+                print(entry)
+                
+                
+                # for entry in calorie_daily_target_data:
+                #     if entry.get('Name') == user_name_input:
+                #         if 'Calories daily' in entry:
+                #             return True, entry['Calories daily']
+                #         else: #if not found, write into file
+                #             with open('calories_daily_target','w', encoding='utf-8') as file:
+                #                 json.dump(calorie_daily_target_data, file, indent=4)
                  
-                print('data match')
+                # print('data match')
 
             
         case 2:
-            typer.echo('Add calorie entry')
+            print('Add calorie entry')
             #add calories to the day & save to json file
-            calories_entry = typer.prompt('Enter calories you would like to add today', type=int)
+            calories_entry = int(input('Enter calories you would like to add today', type=int))
             print(calories_entry)
             today = date.today().isoformat()
             print(today)
@@ -76,20 +92,12 @@ def main():
                 calories_entry_today = json.load(file)
                 print(calories_entry_today)
         case 3:
-            typer.echo('Help')
-            typer.echo('Follow the menu instructions to track and view your calories')
-            return_to_menu = typer.prompt('Enter any key to return to menu')
+            print('Help')
+            print('Follow the menu instructions to track and view your calories')
+            return_to_menu = ('Enter any key to return to menu')
         case 4:
-            typer.echo('Quit')
-
-    
-
-
-        
-        
-        
-    
-    
-
+            print('Quit')
+            
+            
 if __name__ == "__main__":
-    typer.run(main)
+    main()
