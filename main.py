c6fec9d7b41a93649e3f48e034c2578a07c12e78
Bonnie <bonniechiu527@gmail.com>
2024-12-20 10:01:08 +1000

    calories daily target commit

--- main.py
+++ main.py
@@ -20,26 +20,41 @@ def main():
     
     # load user file with state
     with open('user_credentials','r', encoding='utf-8') as file:
-            user_credentials = json.load(file)
+        user_credentials = json.load(file)
             
 
     #main menu options
-    typer.echo('You have [calories]/[target] today.')
+    typer.echo('You have [calories]/{calories_daily_target} today.')
     typer.echo('You have [calories]/[target] this week')
-    typer.echo('Options:\n 1. Set calories target 2.Add calorie entry 3.Help 4.Quit')
+    typer.echo('Options:\n 1.Set calories target \n 2.Add calorie entry \n 3.Help \n 4.Quit')
             
     choice = typer.prompt('Enter option', type=int)
     
     match choice:
         case 1:
             typer.echo('Set calorie target')
+            #set calories target & save to json file
+            calorie_daily_target = typer.prompt('Set your daily target calorie', type=int)
+            calorie_daily_target_data = {
+                'Name':user_name_input,
+                'Calories daily': calorie_daily_target
+            }
+            with open('calories_daily_target','w', encoding='utf-8') as file:
+                json.dump(calorie_daily_target_data, file, indent=4)
+            with open('calories_daily_target','r', encoding='utf-8') as file:
+                calorie_daily_target_data = json.load(file)
+                print(calorie_daily_target_data)
+            
+            
         case 2:
             typer.echo('Add calorie entry')
+            
         case 3:
             typer.echo('Help')
         case 4:
             typer.echo('Quit')
 
+    
 
 
         
