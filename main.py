1b59f8fa4d3ad838877b8bb97025482de1f59a78
Bonnie <bonniechiu527@gmail.com>
2024-12-21 19:18:32 +1000

    calorie entry added

--- main.py
+++ main.py
@@ -62,21 +62,20 @@ def main():
             
             except FileNotFoundError:
             # If file doesn't exist, create it with new data
-                with open('calories_daily_target.json', 'w', encoding='utf-8') as file:
+                with open('calories_daily_target_backup.json', 'w', encoding='utf-8') as file:
                     json.dump(calorie_daily_target_data, file, indent=4)
                     print(f'Calorie target {calorie_daily_target} has been updated.')
             finally: 
                 print('return to main menu')
-                #add method/function to return to menu or quit 
+                #add method/function to return to menu for options
         
         case 2:
             print('Add calorie entry')
             #add calories to the day & save to json file
 
-            calories_entry = int(input('Enter calories you would like to add today: '))
+            calories_entry = int(input('Enter calories you would like to add to today: '))
             print(calories_entry)
             today = date.today().isoformat()
-            print(today)
             
             calories_list = [0] #store calories_entry as an array 
             calories_list.append(calories_entry)
@@ -89,18 +88,15 @@ def main():
                 'Total calories consumed today': total_calories_consumed_today
             }
             try:
-                #read file, if no file
-                with open('total_calories_consumed_today.json','r', encoding='utf-8') as file:
-                    total_calories_consumed_today = json.load(file)
-                    print(f'total calories file found: {total_calories_consumed_today} ')
-                #if no file, make file and add to file   
-            except FileNotFoundError:
-                with open('total_calories_consumed_today.json','w', encoding='utf-8') as file:
+                with open('total_calories_consumed_today.json', 'w', encoding='utf-8') as file:
                     json.dump(total_calories_consumed_today, file, indent=4)
-                    print(f'total_calories_comsumed_today file created: {total_calories_consumed_today}')
-                    
-                print(f'You have successfully added: {calories_entry_today} to your account today.') 
-                print(f'Total calories consumed today: {total_calories_consumed_today}')
+                    print(f'total_calories_comsumed_today file created: {total_calories_consumed_today}')      
+                    print(f'You have successfully added: {calories_entry_today} to your account today.') 
+                    print('You have consumed x amount of calories today')
+
+            except FileNotFoundError:
+                print('oops!')
+
             finally:
                 print('xxxxxxx')#test purposes
                 #to add return to main menu or quit method 
