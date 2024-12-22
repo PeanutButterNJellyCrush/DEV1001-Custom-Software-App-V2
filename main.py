8488a01caedf6bb0fb099c48c2b7629441575766
Paul Chiu <paulchiu@gmail.com>
2024-12-22 14:27:52 +1000

    fix formatting

--- main.py
+++ main.py
@@ -70,33 +70,31 @@ def main():
                 #add method/function to return to menu for options
         
         case 2:
-            calories_daily_target = 2000#would like to change this to inherite from case 1 
-            calories_entry_data = []#put into list
+            calories_daily_target = 2000 #would like to change this to inherite from case 1 
+            calories_entry_data = [] #put into list
             remaining_calories = calories_daily_target #to minus from
 
-            while True:
-                today = date.today().isoformat()#today date
-                calories_entry = int(input('Add to today entry: '))
-                print(calories_entry,today)
-                
-                calories_entry_data.append({
-                    'date': today,
-                    'calories': calories_entry
-                })
-                remaining_calories -= calories_entry
-                    #calorie entry is appended to data
-                print(f'{remaining_calories} for the day.')
-                print(remaining_calories)
-                
-                data = {
-                    'daily target':calories_daily_target,
-                    'entry':calories_entry_data
-                }
-                
-                with open('calories_entry.json','w',encoding='utf-8') as file:
-                    json.dump(data,file)
-                    print('entry has been saved')
-                    break
+            today = date.today().isoformat() #today date
+            calories_entry = int(input('Add to today entry: '))
+            print(calories_entry,today)
+            
+            calories_entry_data.append({
+                'date': today,
+                'calories': calories_entry
+            })
+            remaining_calories -= calories_entry
+
+            print(f'{remaining_calories} for the day.')
+            print(remaining_calories)
+            
+            data = {
+                'daily target':calories_daily_target,
+                'entry':calories_entry_data
+            }
+            
+            with open('calories_entry.json','w',encoding='utf-8') as file:
+                json.dump(data,file)
+                print('entry has been saved')
             
         case 3:
             print('Help')
